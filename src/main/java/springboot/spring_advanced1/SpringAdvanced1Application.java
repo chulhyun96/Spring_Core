ae028966cf3b79aa7e30fcbffab8289bb8960d96
박철현 <tghy4@naver.com>
2024-08-23 19:03:17 +0900

    first commit

---
+++ src/main/java/springboot/spring_advanced1/SpringAdvanced1Application.java
@@ -0,0 +1,13 @@
+package springboot.spring_advanced1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvanced1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvanced1Application.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/spring_advanced1/SpringAdvanced1ApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.spring_advanced1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvanced1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

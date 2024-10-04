c8a94de916f2113f1ca565e0762aff9f86f09123
박철현 <park82602@gmail.com>
2024-10-04 23:15:34 +0900

    JDK 동적 프로그래밍

---
+++ src/test/java/springboot/spring_advanced1/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,55 @@
+package springboot.spring_advanced1.jdkdynamic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.function.Supplier;
+
+@Slf4j
+public class ReflectionTest {
+
+    @Test
+    @DisplayName("reflection0")
+    void reflection0() {
+        Hello target = new Hello();
+
+//        excuteLogging(target::callA);
+//        excuteLogging(target::callB);
+    }
+
+    private void excuteLogging(Supplier<String> method){
+        log.info("start");
+        String result1 = method.get();
+        log.info("result :{}", result1);
+
+    }
+    @Test
+    @DisplayName("")
+    void reflection1() throws Exception {
+        /*클래스의 메타 정볼를 먼저 얻고, 메서드의 정볼를 얻어온다*/
+        String method = "callA";
+        Class<?> aClass = Class.forName("springboot.spring_advanced1.jdkdynamic.ReflectionTest$Hello");
+        Hello hello = new Hello();
+        Method callA = aClass.getMethod(method);
+        dynamicCall(callA, hello);
+    }
+    private void dynamicCall(Method method, Object target) throws Exception {
+        log.info("start");
+        Object result = method.invoke(target);// 차이
+        log.info("result:{}", result);
+    }
+
+    @Slf4j
+    static class Hello{
+        public String callA() {
+            log.info("callA");
+            return "A";
+        }
+        public String callB() {
+            log.info("callB");
+            return "B";
+        }
+    }
+}
